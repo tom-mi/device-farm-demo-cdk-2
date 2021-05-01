affd8f75c271b75261ab45fef7b372917de022a9
Thomas Reifenberger <tom-mi@users.noreply.github.com>
2021-05-01 18:31:24 +0200

    Extract device-farm action

---
+++ infrastructure/src/device-farm-action.ts
@@ -0,0 +1,62 @@
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import { Action } from '@aws-cdk/aws-codepipeline-actions';
+import { ActionArtifactBounds, ActionBindOptions, ActionCategory, ActionConfig, IStage } from '@aws-cdk/aws-codepipeline';
+import { Construct } from '@aws-cdk/core';
+
+interface DeviceFarmActionProps {
+  actionName: string,
+  readonly inputs: codepipeline.Artifact[];
+  recordAppPerformanceData: boolean
+  appType: string
+  projectId: string
+  app: string
+  radioBluetoothEnabled: boolean
+  recordVideo: boolean
+  radioWifiEnabled: boolean
+  radioNfcEnabled: boolean
+  radioGpsEnabled: boolean
+  test: string
+  devicePoolArn: string
+  testType: string
+}
+
+export class DeviceFarmAction extends Action {
+  private readonly props: DeviceFarmActionProps;
+
+  constructor(props: DeviceFarmActionProps) {
+    super({
+      ...props,
+      category: ActionCategory.TEST,
+      provider: 'DeviceFarm',
+      owner: 'AWS',
+      artifactBounds: new class implements ActionArtifactBounds {
+        readonly minInputs: number = 1;
+        readonly maxInputs: number = 1;
+        readonly minOutputs: number = 0;
+        readonly maxOutputs: number = 0;
+      },
+      version: '1',
+    });
+
+    this.props = props;
+  }
+
+  protected bound(_scope: Construct, _stage: IStage, _options: ActionBindOptions): ActionConfig {
+    return {
+      configuration: {
+        RecordAppPerformanceData: this.props.recordAppPerformanceData,
+        AppType: this.props.appType,
+        ProjectId: this.props.projectId,
+        App: this.props.app,
+        RadioBluetoothEnabled: this.props.radioBluetoothEnabled,
+        RecordVideo: this.props.recordVideo,
+        RadioWifiEnabled: this.props.radioWifiEnabled,
+        RadioNfcEnabled: this.props.radioNfcEnabled,
+        RadioGpsEnabled: this.props.radioGpsEnabled,
+        Test: this.props.test,
+        DevicePoolArn: this.props.devicePoolArn,
+        TestType: this.props.testType,
+      },
+    };
+  }
+}
--- infrastructure/src/pipeline.ts
+++ infrastructure/src/pipeline.ts
@@ -1,9 +1,9 @@
 import { CfnOutput, Construct, CustomResource, RemovalPolicy, Stack, StackProps } from '@aws-cdk/core';
-import { ActionArtifactBounds, ActionBindOptions, ActionCategory, ActionConfig, Artifact, IStage, Pipeline } from '@aws-cdk/aws-codepipeline';
-import { Action, CodeBuildAction, S3SourceAction } from '@aws-cdk/aws-codepipeline-actions';
+import { Artifact, Pipeline } from '@aws-cdk/aws-codepipeline';
+import { CodeBuildAction, S3SourceAction } from '@aws-cdk/aws-codepipeline-actions';
 import { Bucket } from '@aws-cdk/aws-s3';
 import { ComputeType, LinuxBuildImage, PipelineProject } from '@aws-cdk/aws-codebuild';
-import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import { DeviceFarmAction } from './device-farm-action';
 
 interface PipelineStackProps {
   projectCustomResourceServiceToken: string
@@ -88,63 +88,5 @@ export class PipelineStack extends Stack {
     new CfnOutput(this, 'SourceBucket', {
       value: source.bucketName,
     });
-  }
-}
-
-interface DeviceFarmActionProps {
-  actionName: string,
-  readonly inputs: codepipeline.Artifact[];
-  recordAppPerformanceData: boolean
-  appType: string
-  projectId: string
-  app: string
-  radioBluetoothEnabled: boolean
-  recordVideo: boolean
-  radioWifiEnabled: boolean
-  radioNfcEnabled: boolean
-  radioGpsEnabled: boolean
-  test: string
-  devicePoolArn: string
-  testType: string
-}
-
-export class DeviceFarmAction extends Action {
-  private readonly props: DeviceFarmActionProps;
-
-  constructor(props: DeviceFarmActionProps) {
-    super({
-      ...props,
-      category: ActionCategory.TEST,
-      provider: 'DeviceFarm',
-      owner: 'AWS',
-      artifactBounds: new class implements ActionArtifactBounds {
-        readonly minInputs: number = 1;
-        readonly maxInputs: number = 1;
-        readonly minOutputs: number = 0;
-        readonly maxOutputs: number = 0;
-      },
-      version: '1',
-    });
-
-    this.props = props;
-  }
-
-  protected bound(_scope: Construct, _stage: IStage, _options: ActionBindOptions): ActionConfig {
-    return {
-      configuration: {
-        RecordAppPerformanceData: this.props.recordAppPerformanceData,
-        AppType: this.props.appType,
-        ProjectId: this.props.projectId,
-        App: this.props.app,
-        RadioBluetoothEnabled: this.props.radioBluetoothEnabled,
-        RecordVideo: this.props.recordVideo,
-        RadioWifiEnabled: this.props.radioWifiEnabled,
-        RadioNfcEnabled: this.props.radioNfcEnabled,
-        RadioGpsEnabled: this.props.radioGpsEnabled,
-        Test: this.props.test,
-        DevicePoolArn: this.props.devicePoolArn,
-        TestType: this.props.testType,
-      },
-    };
   }
 }
